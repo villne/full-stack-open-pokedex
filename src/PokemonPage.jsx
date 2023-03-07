1944c0dd554951233d6ea345861bc0e44a6d8f3d
villne <ville.niemenmaa@gmail.com>
2023-03-07 17:20:59 +0200

    Exercise 11.8 | fix next pokemon page

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
-import ErrorMessage from "./ErrorMessage";
-import LoadingSpinner from "./LoadingSpinner";
-import PokemonAbility from "./PokemonAbility";
-import { useApi } from "./useApi";
+import ErrorMessage from './ErrorMessage'
+import LoadingSpinner from './LoadingSpinner'
+import PokemonAbility from './PokemonAbility'
+import { useApi } from './useApi'
 
-const formatName = (nameWithDash) => nameWithDash.replace("-", " ");
+const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
 
 const PokemonPage = ({ previous, next }) => {
-  const { name } = useParams();
+  const { name } = useParams()
   const {
     data: pokemon,
     error,
     isLoading,
-  } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
   if (isLoading) {
-    return <LoadingSpinner />;
+    return <LoadingSpinner />
   }
   if (error) {
-    return <ErrorMessage error={error} />;
+    return <ErrorMessage error={error} />
   }
 
-  const { type } = pokemon.types.find((type) => type.slot === 1);
+  const { type } = pokemon.types.find((type) => type.slot === 1)
   const stats = pokemon.stats
     .map((stat) => ({
       name: formatName(stat.stat.name),
       value: stat.base_stat,
     }))
-    .reverse();
-  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden);
+    .reverse()
+  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find(
     (ability) => ability.is_hidden === true
-  );
+  )
 
-  console.log("hiddenAbility=", hiddenAbility);
+  console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
       <div className="links">
@@ -77,7 +77,7 @@ const PokemonPage = ({ previous, next }) => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default PokemonPage;
+export default PokemonPage
