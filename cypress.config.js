987382e6114323abead77a04a0f38956da4c27bc
villne <ville.niemenmaa@gmail.com>
2023-03-08 23:15:38 +0200

    11.9 Simple end to end -tests #1

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
@@ -7,4 +7,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+})
