8670379e3142986dcb34b2a09073027eec23c3a0
villne <ville.niemenmaa@gmail.com>
2023-03-20 01:05:25 +0200

    Make fly.io health fail

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
