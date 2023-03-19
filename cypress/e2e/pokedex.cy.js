d6d0e861d37a8facca5cce38f5540148fadc06d9
villne <ville.niemenmaa@gmail.com>
2023-03-20 01:23:44 +0200

    revert pokedex test port number

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
-describe("Pokedex", function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:8080");
-    cy.contains("ivysaur");
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
-  });
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
 
-  it("pokemon page can be navigated to", function () {
-    cy.visit("http://localhost:5000");
-    cy.contains("pikachu").click();
-    cy.get("[class=pokemon-name]").should("have.text", "pikachu");
-  });
-});
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').click()
+    cy.get('[class=pokemon-name]').should('have.text', 'pikachu')
+  })
+})
